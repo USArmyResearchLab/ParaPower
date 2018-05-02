600ff49946c29d3d6c37f6c061580ac4163a18ed
Morris Berman (Surface) <morris.s.berman.civ@mail.mil>
2018-05-02 08:22:18 -0400

    Directories removed from path.

--- 3D Thermal Parameters/Working/Run_ParaPower.m
+++ 3D Thermal Parameters/Working/Run_ParaPower.m
@@ -4,9 +4,10 @@ if exist('PARAPowerGUI.m','file')==2
     movefile('PARAPowerGUI.m.tmp','ParaPowerGUI.m')
 end
 
-addpath('..');
-addpath('../Thermal');
-addpath('../Transient');
-addpath('../working');
-addpath('../Stress Model 2.0');
+disp('The code has modified so that all required modules should be in the "working" directory without a need to addpath.')
+% addpath('..');
+% addpath('../Thermal');
+% addpath('../Transient');
+% addpath('../working');
+% addpath('../Stress Model 2.0');
 H=ParaPowerGUI;
