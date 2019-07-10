b94f66696221b7473c6cae0af4b649fa8a3dc28f
Morris Berman (Surface) <morris.s.berman.civ@mail.mil>
2019-07-10 13:00:49 -0400

    Fixed FormModel to separate

--- ARL_ParaPower/FormModel.m
+++ ARL_ParaPower/FormModel.m
@@ -108,7 +108,7 @@ Z0=[]; %Z coordinates with zero thickness
 
 %Go through features list and determine if it's a zero thickness in any
 %direction. Build up X, Y, Z list of coordinates and X0, Y0, Z0 which is
-%the special list that covers zero thickness features.
+%the special list that covers zero thickness features and user defined locations.
 MinFeatureSize=[1 1 1]*inf;
 for i=1:length(Features)
     Fd=Features(i).x;
@@ -133,25 +133,31 @@ for i=1:length(Features)
     Features(i).y=sort(Features(i).y);
     Features(i).z=sort(Features(i).z);
 
+    %Store X coord values in X0 (user defined values) and X (Prog/division defined coords)
     Coords=linspace(Features(i).x(1), Features(i).x(2), 1+Features(i).dx);
     X0=[X0 Coords(1) Coords(end)];
     if Features(i).x(1)~=Features(i).x(2)
         X =[X Coords(2:end-1)];
         MinFeatureSize(1)=min(MinFeatureSize(1),min(Coords(2:end)-Coords(1:end-1)));
     end
+    
+    %Store Y coord values in X0 (user defined values) and X (Prog/division defined coords)
     Coords=linspace(Features(i).y(1), Features(i).y(2), 1+Features(i).dy);
     Y0=[Y0 Coords(1) Coords(end)];
     if Features(i).y(1)~=Features(i).y(2)
         Y =[Y Coords(2:end-1)];
-        MinFeatureSize(1)=min(MinFeatureSize(1),min(Coords(2:end)-Coords(1:end-1)));
+        MinFeatureSize(2)=min(MinFeatureSize(2),min(Coords(2:end)-Coords(1:end-1)));
     end
+    
+    %Store Z coord values in X0 (user defined values) and X (Prog/division defined coords)
     Coords=linspace(Features(i).z(1), Features(i).z(2), 1+Features(i).dz);
     Z0=[Z0 Coords(1) Coords(end)];
     if Features(i).z(1)~=Features(i).z(2)
         Z =[Z Coords(2:end-1)];
-        MinFeatureSize(1)=min(MinFeatureSize(1),min(Coords(2:end)-Coords(1:end-1)));
+        MinFeatureSize(3)=min(MinFeatureSize(3),min(Coords(2:end)-Coords(1:end-1)));
     end
-%     if Features(i).x(1)==Features(i).x(2) %Acount for special case of zero height layer
+    
+    %     if Features(i).x(1)==Features(i).x(2) %Acount for special case of zero height layer
 %         X0=[X0 Features(i).x(1) Features(i).x(2)];
 %         X=[X Features(i).x(1)];
 %     else
@@ -159,27 +165,25 @@ for i=1:length(Features)
 %         MinFeatureSize(1)=min(MinFeatureSize(1),min(Coords(2:end)-Coords(1:end-1)));
 %         X=[X Coords];
 %     end
-    if Features(i).y(1)==Features(i).y(2) %Acount for special case of zero height layer
-        Y0=[Y0 Features(i).y(1)];
-        Y=[Y Features(i).y(1)];
-    else
-        Coords=linspace(Features(i).y(1), Features(i).y(2), 1+Features(i).dy);
-        MinFeatureSize(2)=min(MinFeatureSize(2),min(Coords(2:end)-Coords(1:end-1)));
-        Y=[Y Coords];
-    end
-    if Features(i).z(1)==Features(i).z(2) %Acount for special case of zero height layer
-        Z0=[Z0 Features(i).z(1)];
-        Z=[Z Features(i).z(1)];
-    else
-        Coords=linspace(Features(i).z(1), Features(i).z(2), 1+Features(i).dz);  
-        MinFeatureSize(3)=min(MinFeatureSize(3),min(Coords(2:end)-Coords(1:end-1)));
-        Z=[Z Coords];
-    end
+%     if Features(i).y(1)==Features(i).y(2) %Acount for special case of zero height layer
+%         Y0=[Y0 Features(i).y(1)];
+%         Y=[Y Features(i).y(1)];
+%     else
+%         Coords=linspace(Features(i).y(1), Features(i).y(2), 1+Features(i).dy);
+%         MinFeatureSize(2)=min(MinFeatureSize(2),min(Coords(2:end)-Coords(1:end-1)));
+%         Y=[Y Coords];
+%     end
+%     if Features(i).z(1)==Features(i).z(2) %Acount for special case of zero height layer
+%         Z0=[Z0 Features(i).z(1)];
+%         Z=[Z Features(i).z(1)];
+%     else
+%         Coords=linspace(Features(i).z(1), Features(i).z(2), 1+Features(i).dz);  
+%         MinFeatureSize(3)=min(MinFeatureSize(3),min(Coords(2:end)-Coords(1:end-1)));
+%         Z=[Z Coords];
+%     end
 end
 
-%Collapse X and y completely.  Maintain zero thickness layers for dz
-%Fix floating point inaccuracies.  Round to accuracy of epsilon - 2
-%decimals
+%Collapse user specifice locations to accuracy of epsilon - 2 decimals
 if ~isempty(X0)
     X0=unique(round(X0,floor(abs(log10(100*eps(max(X0)))))));
 end
@@ -191,7 +195,7 @@ if ~isempty(Z0)
 end
 
 %Set the tolerance to be two orders of magnitude less than min individ
-%features size
+%features size for program specified coordinates
 MinFeatureSize=2+floor(abs(floor(min(0,log10(MinFeatureSize)))));
 
 X=unique(round(X,MinFeatureSize(1)));
@@ -201,9 +205,11 @@ Z=unique(round(Z,MinFeatureSize(3)));
 % Y=unique(round(Y,floor(abs(log10(100*eps(max(Y)))))));
 % Z=unique(round(Z,floor(abs(log10(100*eps(max(Z)))))));
 
-X=sort([X X0]); 
-Y=sort([Y Y0]); 
-Z=sort([Z Z0]); 
+%Combine user specified and program specified coordinates into single list
+%with a tolerance of 1e-10 * abs(max(X|Y|Z)
+X=uniquetol([X X0],1e-10); 
+Y=uniquetol([Y Y0],1e-10); 
+Z=uniquetol([Z Z0],1e-10); 
 
 %Create list of final Delta coordinates that will be used to generate
 %model.
