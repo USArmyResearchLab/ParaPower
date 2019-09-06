1baff8fdcf995a05c07231bf58c21c06d8b86366
Morris Berman (Surface) <morris.s.berman.civ@mail.mil>
2019-09-06 10:23:16 -0400

    Bug: Fixed matlib gen cases for empty variable list

--- ARL_ParaPower/PPMatLib.m
+++ ARL_ParaPower/PPMatLib.m
@@ -1007,7 +1007,7 @@ classdef PPMatLib < handle
                                 ME.getReport
                                 ThisPropVal=NaN;
                             end
-                                                                                                                                                                                           %fprintf('%s: %s\n',ThisMat.Name,ThisPropName)  %MSB
+                                                                                                                                                      %fprintf('%s: %s\n',ThisMat.Name,ThisPropName)  %MSB
                             ScalarValue=length(ThisPropVal(:))==1; %If there is a single value it will be placed as a scalar not cell array
                             if OrigChar || length(ThisPropVal{Ipv})~=1  %If the value changed on eval, then cycle through mats
                                 NewMatLib=obj.ExpandMatLib(NewMatLib, ThisPropVal{Ipv}, Imat, ThisPropName, Ipv, ScalarValue);
@@ -1026,15 +1026,17 @@ function OutVar=ProtectedEval(InString, VarList)
     ErrText='';
     OutVar=[];
     EvalText='';
-    for Ivar=1:length(VarList(:,1))
-        if exist(VarList{Ivar,1},'var')
-            Stxt=sprintf('''%s'' variable already exists in the namespace. Please change your variable name.\n',VarName);
-            ErrText=[ErrText Stxt];
-        else
-            if ischar(VarList{Ivar,2})
-                EvalText=[EvalText VarList{Ivar,1} '=' VarList{Ivar,2} ';'];
+    if ~isempty(VarList)
+        for Ivar=1:length(VarList(:,1))
+            if exist(VarList{Ivar,1},'var')
+                Stxt=sprintf('''%s'' variable already exists in the namespace. Please change your variable name.\n',VarName);
+                ErrText=[ErrText Stxt];
             else
-                EvalText=[EvalText VarList{Ivar,1} '=VarList{' num2str(Ivar) ',2};'];
+                if ischar(VarList{Ivar,2})
+                    EvalText=[EvalText VarList{Ivar,1} '=' VarList{Ivar,2} ';'];
+                else
+                    EvalText=[EvalText VarList{Ivar,1} '=VarList{' num2str(Ivar) ',2};'];
+                end
             end
         end
     end
