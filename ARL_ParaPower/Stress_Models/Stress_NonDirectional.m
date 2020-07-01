d54fe0c2c5174d838201b7926fc48bcfa7396800
Morris Berman (Surface) <morris.s.berman.civ@mail.mil>
2020-07-01 07:58:51 -0400

    Starting Point for New Stress Model Incorporation

---
+++ ARL_ParaPower/Stress_Models/Stress_NonDirectional.m
@@ -0,0 +1,27 @@
+%Shell to call the Miner non-directional model
+function Stress=Stress_NonDirectional(Results)
+
+    MFPath=[strrep(mfilename('fullpath'),mfilename,'') 'NonDirectional'];
+    OldPath=addpath(MFPath);
+    
+    ErrText=[];
+    ModelInput=Results.Model;
+    if ~isempty(find(strcmpi(Results.StatesAvail,'Thermal')))
+        Tres=Results.getState('Thermal');
+    else
+        ErrText=[ErrText 'No thermal state exists for computing stress.' newline ];
+    end
+
+    if ~isempty(ErrText)
+        Stress=ErrText(1:end-1);
+        return
+    else
+        
+        [StressX,StressY,StressZ]=Stress_NoSubstrate1(Results);        
+        path(OldPath);
+        Stress.X=StressX;
+        Stress.Y=StressY;
+        Stress.Z=StressZ;
+    end
+
+end
